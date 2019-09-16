9cea662be84ba9229cd7a31972d1cdaef4353fb9
NinjaFrog <milos281295@gmail.com>
2019-09-16 10:32:58 +0200

    Initial Landscape Created

---
+++ UnrealBattleTanks/Source/UnrealBattleTanks/UnrealBattleTanksGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UnrealBattleTanksGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALBATTLETANKS_API AUnrealBattleTanksGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
